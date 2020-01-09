1935ca5c0ec2466c09f4e5cfed4c1c25be990084
Frederick Klyk <klyk@adesso-mobile.de>
2020-01-09 16:11:39 +0100

    master: update ktor client call

--- app/src/main/java/de/adesso_mobile/coroutinesadvanced/io/network/WeatherService.kt
+++ app/src/main/java/de/adesso_mobile/coroutinesadvanced/io/network/WeatherService.kt
@@ -2,15 +2,12 @@ package de.adesso_mobile.coroutinesadvanced.io.network
 
 import io.ktor.client.HttpClient
 import io.ktor.client.request.get
-import io.ktor.client.request.url
-import io.ktor.client.response.HttpResponse
+import io.ktor.client.statement.HttpStatement
 
 
 class WeatherService(val client: HttpClient, val baseUrl: String) {
 
-    suspend fun fetchWeather() = client.get<HttpResponse>("$baseUrl/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22"){
-        
-    }
+    suspend fun fetchWeather() = client.get<HttpStatement>("$baseUrl/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22").execute()
 
     data class Weather(val coord: Coordinate, val base: String)
 
--- app/src/main/java/de/adesso_mobile/coroutinesadvanced/ui/coroutines/CoroutinesFragmentViewModel.kt
+++ app/src/main/java/de/adesso_mobile/coroutinesadvanced/ui/coroutines/CoroutinesFragmentViewModel.kt
@@ -4,16 +4,13 @@ import androidx.lifecycle.viewModelScope
 import de.adesso_mobile.coroutinesadvanced.io.network.WeatherService
 import de.adesso_mobile.coroutinesadvanced.ui.base.BaseViewModel
 import io.ktor.client.call.receive
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.*
 import timber.log.Timber
 
 class CoroutinesFragmentViewModel(private val weatherApi: WeatherService) : BaseViewModel() {
 
 
     fun initialize() = viewModelScope.launch {
-
         Timber.d("HTTP:")
         try {
             withContext(Dispatchers.IO) { weatherApi.fetchWeather() }.apply {
@@ -30,5 +27,4 @@ class CoroutinesFragmentViewModel(private val weatherApi: WeatherService) : Base
             Timber.e(e)
         }
     }
-
 }
