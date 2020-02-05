09bb10a9e19ab3e007682d21edc7b3bdf14da12f
Frederick Klyk <klyk@adesso-mobile.de>
2020-02-05 10:34:53 +0100

    master: nav graph argument test

--- app/src/main/java/de/adesso_mobile/coroutinesadvanced/ui/main/DummyFragment2.kt
+++ app/src/main/java/de/adesso_mobile/coroutinesadvanced/ui/main/DummyFragment2.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import de.adesso_mobile.coroutinesadvanced.R
 import de.adesso_mobile.coroutinesadvanced.ui.base.BaseFragment
+import timber.log.Timber
 
 class DummyFragment2 : BaseFragment() {
 
@@ -14,7 +15,9 @@ class DummyFragment2 : BaseFragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+
+        val myArg = arguments?.get("test")
+        Timber.d("my Arg: $myArg")
         return inflater.inflate(R.layout.dummy2_fragment, container, false)
     }
 }
