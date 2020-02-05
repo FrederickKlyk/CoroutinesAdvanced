7bf40e3f30c90e3abe4451d9ee167bec7af2415b
Frederick Klyk <klyk@adesso-mobile.de>
2020-02-05 11:15:01 +0100

    master: ViewPager2PageTransition, http catch refactor

--- app/src/main/java/de/adesso_mobile/coroutinesadvanced/ui/coroutines/CoroutinesFragmentViewModel.kt
+++ app/src/main/java/de/adesso_mobile/coroutinesadvanced/ui/coroutines/CoroutinesFragmentViewModel.kt
@@ -25,23 +25,34 @@ class CoroutinesFragmentViewModel(
     val postDataResponseText = MutableLiveData("")
 
     fun initialize() = viewModelScope.launch {
-        Timber.d("HTTP:")
-        withContext(Dispatchers.IO) { weatherApi.fetchWeather() }.apply {
-            if (status.value == 200) {
-                Timber.d("${status.value}: ${status.description}")
+        try {
+            withContext(Dispatchers.IO) { weatherApi.fetchWeather() }
+                .apply {
+                    if (status.value == 200) {
+                        Timber.d("${status.value}: ${status.description}")
 
-                val weather = receive<WeatherService.Weather>()
-                Timber.d(weather.toString())
-                val p1 = weather.copy(base = "test")
-                Timber.d(p1.toString())
+                        val weather = receive<WeatherService.Weather>()
+                        Timber.d(weather.toString())
+                        val p1 = weather.copy(base = "test")
+                        Timber.d(p1.toString())
 
-                wetterDatenText.value = "Basis: ${weather.base} \n${weather.coord}\nTemperatur: ${String.format(
-                    "%.2f",
-                    weather.main.temp - 273.15
-                )}°c \nWindstufe: ${weather.wind.speed}"
-            } else {
-                wetterDatenText.value = "Fehler beim Laden mit Statuscode ${status.value}, ${status.description} \n ${this.content.readUTF8Line()}"
-            }
+                        wetterDatenText.value = "Basis: ${weather.base} \n${weather.coord}\nTemperatur: ${String.format(
+                            "%.2f",
+                            weather.main.temp - 273.15
+                        )}°c \nWindstufe: ${weather.wind.speed}"
+                    } else {
+                        wetterDatenText.value =
+                            "Fehler beim Laden mit Statuscode ${status.value}, ${status.description} \n ${this.content.readUTF8Line()}"
+                    }
+                }
+        } catch (e: NoTransformationFoundException) {
+            Timber.e("sendHTTPPost(), NoTransformationFoundException: $e")
+        } catch (e: SocketTimeoutException) {
+            Timber.e("sendHTTPPost(), SocketTimeoutException: $e")
+        } catch (e: IOException) {
+            Timber.e("sendHTTPPost(), IOException: $e")
+        } catch (e: Exception) {
+            Timber.e("sendHTTPPost(), Exception: $e")
         }
     }
 
---
+++ app/src/main/java/de/adesso_mobile/coroutinesadvanced/ui/viewpager2/Viewpager2PageTransition.kt
@@ -0,0 +1,58 @@
+package de.adesso_mobile.coroutinesadvanced.ui.viewpager2
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.viewpager2.widget.ViewPager2
+import de.adesso_mobile.coroutinesadvanced.databinding.Viewpager2FragmentBinding
+import de.adesso_mobile.coroutinesadvanced.ui.base.BaseFragment
+import kotlinx.android.synthetic.main.viewpager2_fragment.*
+import org.koin.android.ext.android.inject
+
+class Viewpager2PageTransition : BaseFragment() {
+
+    val viewModel: Viewpager2SharedViewModel by inject()
+    private val adapter: CategoryAdapter by lazy { CategoryAdapter() }
+    private lateinit var onPageChangeCallback: OnPageChangeCallback
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return Viewpager2FragmentBinding.inflate(inflater).apply {
+            viewModel = viewModel
+            lifecycleOwner = this@Viewpager2PageTransition
+        }.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupViewPager2()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        viewPager2.unregisterOnPageChangeCallback(onPageChangeCallback)
+    }
+
+    private fun setupViewPager2() {
+        viewPager2.adapter = adapter
+        adapter.setItem(viewModel.categories.value)
+
+        onPageChangeCallback = OnPageChangeCallback(viewModel.categories.value.size - 1) {
+            viewPager2.currentItem = it
+        }
+        viewPager2.registerOnPageChangeCallback(onPageChangeCallback)
+    }
+}
+
+class OnPageChangeCallback(private val lastItem: Int, private val listener: (Int) -> Unit) : ViewPager2.OnPageChangeCallback() {
+
+    override fun onPageSelected(position: Int) {
+        super.onPageSelected(position)
+
+        when (position) {
+            0 -> listener.invoke(lastItem)
+        }
+    }
+}
