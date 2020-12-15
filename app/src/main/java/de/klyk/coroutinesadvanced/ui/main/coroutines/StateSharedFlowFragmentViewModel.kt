f535adf20087828a0a70fcee3ae90fcb4897d5ce
Frederick Klyk <klyk@adesso-mobile.de>
2020-12-15 09:57:39 +0100

    master: naming of methods

--- app/src/main/java/de/klyk/coroutinesadvanced/ui/main/coroutines/StateSharedFlowFragmentViewModel.kt
+++ app/src/main/java/de/klyk/coroutinesadvanced/ui/main/coroutines/StateSharedFlowFragmentViewModel.kt
@@ -16,23 +16,25 @@ class StateSharedFlowFragmentViewModel : BaseViewModel() {
     val myStateFlowCounter: StateFlow<Int> = _myStateFlowCounter
     val nameStateFlow = MutableStateFlow("")
 
-    private val _nameSharedFlow = MutableSharedFlow<String>(onBufferOverflow = BufferOverflow.DROP_LATEST, replay = 2)
+    private val _nameSharedFlow = MutableSharedFlow<String>(onBufferOverflow = BufferOverflow.DROP_LATEST, replay = 0, extraBufferCapacity = 1)
     val nameSharedFlow: SharedFlow<String> = _nameSharedFlow
+
+    /** StateFlow*/
+    fun onStateFlowButtonEmitClicked() {
+        nameStateFlow.value = nameLiveData.value ?: ""
+    }
 
     fun incrementStateFlowCounter() {
         _myStateFlowCounter.value++
     }
 
-    fun onStateFlowButtonClicked() {
-        nameStateFlow.value = nameLiveData.value ?: ""
-    }
-
-    fun onSharedFlowButtonClicked() {
+    /** SharedFlow*/
+    fun onSharedFlowButtonEmitClicked() {
         _nameSharedFlow.tryEmit(nameLiveData2.value ?: "")
         nameLiveData2.value = ""
     }
 
-    fun onSharedFlowButton2Clicked() {
+    fun onSharedFlowButtonSubscribe() {
         viewModelScope.launch {
             nameSharedFlow.collect {
                 sharedFlowText.value += "$it --> "
